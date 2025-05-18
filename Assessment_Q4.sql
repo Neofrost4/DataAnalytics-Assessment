961846a00370a164476d62cf8f0bdf654bc6f980
Neofrost4 <newworldosemwegie@yahoo.com>
2025-05-18 21:33:18 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,1 +1,11 @@
-
+SELECT 
+    u.id AS customer_id,
+    u.first_name as name,
+    TIMESTAMPDIFF(MONTH, u.date_joined, NOW()) AS tenure_months,
+    SUM(s.confirmed_amount) AS total_transactions,
+    ROUND(((SUM(s.confirmed_amount) / NULLIF(TIMESTAMPDIFF(MONTH, u.date_joined, NOW()), 0)) * 12 * 0.001), 2) AS estimated_clv
+FROM users_customuser u
+JOIN savings_savingsaccount s ON u.id = s.owner_id
+WHERE s.confirmed_amount > 0
+GROUP BY u.id, u.name, u.date_joined
+ORDER BY estimated_clv DESC;
